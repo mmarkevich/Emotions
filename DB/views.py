a12662ab263ff84c037055bfdddd245be730ed3b
Dan Dan <den201120@gmail.com>
2021-06-03 02:44:05 +0300

    short cut chair

--- DB/views.py
+++ DB/views.py
@@ -3,8 +3,10 @@ from rest_framework import generics
 from .models import User, VideoInformation, DataAboutUserAndVideo
 from DB.sterializers import UserSerializer, VideoInformationSerializer, DataAboutUserAndVideoSerializer
 from .forms import UserForm
-
-
+from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from streamapp.camera import VideoCamera
+import time
 
 class UserView(generics.ListAPIView):
     queryset = User.objects.all()
@@ -19,7 +21,6 @@ class VideoInformationSerializerView(generics.ListAPIView):
 class DataAboutUserAndVideoSerializerView(generics.CreateAPIView):
     queryset = DataAboutUserAndVideo.objects.all()
     serializer_class = DataAboutUserAndVideoSerializer
-
 
 
 def registration(request):
@@ -41,9 +42,26 @@ def registration(request):
     return render(request, 'DB/registration.html', data)
 
 
+def gen(camera):
+    sec = 0
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+        predicted_emotion = 'alhamduela'
+        time.sleep(1)
+        sec += 1
+
+        if sec % 5 == 0 and sec > 4:
+            VideoCamera.throwData()
+
+def video_feed(request):
+    return StreamingHttpResponse(gen(VideoCamera()),
+                                 content_type='multipart/x-mixed-replace; boundary=frame')
+
+
 def video_list(request):
-
     link = VideoInformation.objects.all()
-
 
     return render(request, 'DB/video_list.html', {'link': link})
--- streamapp/camera.py
+++ streamapp/camera.py
@@ -7,10 +7,10 @@ from keras.preprocessing.image import img_to_array
 from DB.models import DataAboutUserAndVideo
 import copy
 import time
+from PIL import Image
 
-
-model = model_from_json(open("/Users/andreimarkevich/PycharmProjects/FallbackOptionforFER/streamapp/fer2013_model6.json", "r").read())
-model.load_weights('/Users/andreimarkevich/PycharmProjects/FallbackOptionforFER/streamapp/fer2013_model6.h5')
+model = model_from_json(open(r"C:\Users\Asus\PycharmProjects\Emotions4\streamapp\fer2013_model6.json", "r").read())
+model.load_weights(r'C:\Users\Asus\PycharmProjects\Emotions4\streamapp\fer2013_model6.h5')
 
 face_detection_videocam = cv2.CascadeClassifier(os.path.join(
 			settings.BASE_DIR,'opencv_haarcascade_data/haarcascade_frontalface_default.xml'))
@@ -18,6 +18,7 @@ face_detection_videocam = cv2.CascadeClassifier(os.path.join(
 emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
 class VideoCamera(object):
+
 	def __init__(self):
 		self.video = cv2.VideoCapture(0)
 
@@ -29,7 +30,7 @@ class VideoCamera(object):
 		# We are using Motion JPEG, but OpenCV defaults to capture raw images,
 		# so we must encode it into JPEG in order to correctly display the
 		# video stream.
-		sec = 0
+		sec=0
 		gray = cv2.cvtColor(cap_img, cv2.COLOR_BGR2GRAY)
 		faces_detected = face_detection_videocam.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 		for (x, y, w, h) in faces_detected:
@@ -47,15 +48,15 @@ class VideoCamera(object):
 
 			predicted_emotion = emotions[res]
 
-			time.sleep(1)
-			sec += 1
 
-			if sec % 5 == 0 and sec > 4:
-				a = DataAboutUserAndVideo(ID_user = 2, ID_screenshot=3, ID_video_information=3, dominant_emotion=predicted_emotion, edited_dominant_emotion=predicted_emotion)
-				a.save()
 
-				cv2.putText(cap_img, predicted_emotion, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 0, 0), 3,
-						cv2.LINE_AA)
+
+			cv2.putText(cap_img, predicted_emotion, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (200, 0, 0), 3,cv2.LINE_AA)
 #		frame_flip = cv2.flip(cap_img, 1)
 		ret, jpeg = cv2.imencode('.jpg', cap_img)
+
 		return jpeg.tobytes()
+	def throwData(self):
+		a = DataAboutUserAndVideo(ID_user=2, ID_screenshot=3, ID_video_information=3,
+								  dominant_emotion=predicted_emotion, edited_dominant_emotion=predicted_emotion)
+		a.save()
